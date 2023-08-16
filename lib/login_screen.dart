11656a8adafc53b68027d2edbf3ae2700ae2beac
Ali-Shan11223 <jonah2114@gmail.com>
2023-08-16 02:57:20 -0700

    third commit

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -9,24 +9,79 @@ class LogInScreen extends StatefulWidget {
 }
 
 class _LogInScreenState extends State<LogInScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: InkWell(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.zero,
+                  hintText: 'Email',
+                  prefixIcon: const Icon(Icons.email_outlined),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blueGrey.shade300),
+                      borderRadius: BorderRadius.circular(10)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blueGrey.shade300),
+                      borderRadius: BorderRadius.circular(10))),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: passController,
+              keyboardType: TextInputType.name,
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.zero,
+                  hintText: 'Name',
+                  prefixIcon: const Icon(Icons.password_outlined),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blueGrey.shade300),
+                      borderRadius: BorderRadius.circular(10)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blueGrey.shade300),
+                      borderRadius: BorderRadius.circular(10))),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: ageController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.zero,
+                  hintText: 'Age',
+                  prefixIcon: const Icon(Icons.account_box),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blueGrey.shade300),
+                      borderRadius: BorderRadius.circular(10)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blueGrey.shade300),
+                      borderRadius: BorderRadius.circular(10))),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            InkWell(
               onTap: () async {
-                SharedPreferences sp = await SharedPreferences.getInstance();
-                sp.setString('Email', 'jonah2114@gmail.com');
-                sp.setString('Name', 'Ali Shan');
-                sp.setInt('Age', 25);
-                print(sp.getString('Email'));
-                print(sp.getString('Name'));
-                print(sp.getInt('Age').toString());
+                // SharedPreferences sp = await SharedPreferences.getInstance();
+                // sp.setString('Email', 'jonah2114@gmail.com');
+                // sp.setString('Name', 'Ali Shan');
+                // sp.setInt('Age', 25);
+                // print(sp.getString('Email'));
+                // print(sp.getString('Name'));
+                // print(sp.getInt('Age').toString());
               },
               child: Container(
                 height: 50,
@@ -35,9 +90,9 @@ class _LogInScreenState extends State<LogInScreen> {
                     borderRadius: BorderRadius.circular(10)),
                 child: const Center(child: Text('LogIn')),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       )),
     );
   }
